5ff4c56ed52ae1426a2c515cb63e606dfd8748c4
DariiaSkoryk <dariyaskoryk@gmail.com>
2020-06-26 18:15:36 +0300

    Function getErrorMessage(char) was realised

--- ImageConverter.cpp
+++ ImageConverter.cpp
@@ -16,8 +16,17 @@ char ImageConverter::getLastError()
 
 std::string ImageConverter::getErrorMessage(char errorCode = m_errorCode)
 {
-	m_errorCode = ErrorValue::FUNCTION_IS_NOT_REALIZED;
-	return "";
+	std::string message;
+	switch (errorCode) {
+		case ErrorValue::NO_ERROR: message = "";
+		case ErrorValue::FILE_DAMAGED: message = "File was damaged";
+		case ErrorValue::FILE_IS_NOT_SUPPORTED: message = "This file is not supported";
+		case ErrorValue::FILE_NOT_OPENED: message = "This file can`t be opened";
+		case ErrorValue::FILE_NOT_READED: message = "You should read the file before convertation";
+		case ErrorValue::FUNCTION_IS_NOT_REALIZED: message = "Some function is not realised yet";
+		default:message = "Unknown error";
+	}
+	return message;
 }
 
 bool ImageConverter::readFile(std::string)
